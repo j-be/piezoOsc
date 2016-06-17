8272bd8937bfb3d1d1a11ad84e1c856eca1cca03
Juri Berlanda <juriberlanda@hotmail.com>
2016-06-17 16:55:34 +0200

    [Refactoring] Introduce DrumPad class for handling one piezo.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- piezoOsc.ino
+++ piezoOsc.ino
@@ -1,7 +1,8 @@
 #include <OSCMessage.h>
 
-#define OSC_ADDRESS "/drum/0/"
 #define BAUD_RATE 115200
+#define NUM_PADS 4
+#define ADDRESS_BUFFER_LENGTH 20
 
 //Teensy and Leonardo variants have special USB serial
 #if defined(CORE_TEENSY)|| defined(__AVR_ATmega32U4__)
@@ -12,6 +13,35 @@ SLIPEncodedUSBSerial SLIPSerial(Serial);
 #include <SLIPEncodedSerial.h>
 SLIPEncodedSerial SLIPSerial(Serial);
 #endif
+
+String baseAddress = "/drum/";
+OSCMessage msg;
+char charBuffer[ADDRESS_BUFFER_LENGTH];
+
+void outputBang(int value, int padIndex) {
+  String fullAddress = baseAddress + padIndex + "/bang";
+  sendInt(value, fullAddress);
+}
+
+void outputVolume(int value, int padIndex) {
+  String fullAddress = baseAddress + padIndex + "/volume";
+  sendInt(value, fullAddress);
+}
+
+void sendInt(int value, String address) {
+  if (value < 10)
+    return;
+
+  address.toCharArray(charBuffer, ADDRESS_BUFFER_LENGTH);
+
+  msg.setAddress(charBuffer);
+  msg.add((int32_t)value);
+
+  SLIPSerial.beginPacket();
+  msg.send(SLIPSerial); // send the bytes to the SLIP stream
+  SLIPSerial.endPacket(); // mark the end of the OSC Packet
+  msg.empty(); // free space occupied by message
+}
 
 class Envelope {
   private:
@@ -32,14 +62,16 @@ class Envelope {
     }
 };
 
-class BangEnvelope : Envelope {
+class BangEnvelope : public Envelope {
   private:
     boolean aimed = true;
     int aimThreshold = 2;
 
   public:
     virtual boolean isLocalMaximum(int newValue) {
-      if(this->aimed && Envelope::isLocalMaximum(newValue)) {
+      boolean localMax = Envelope::isLocalMaximum(newValue);
+
+      if(this->aimed && localMax) {
         this->aimed = false;
         return true;
       }
@@ -56,50 +88,43 @@ class BangEnvelope : Envelope {
     }
 };
 
-int oldValue;
-Envelope volumeEnv;
-BangEnvelope bangEnv;
+class DrumPad {
+private:
+  int analogPort;
+  int oldValue;
+  Envelope volumeEnv;
+  BangEnvelope bangEnv;
 
-OSCMessage msg;
+public:
+  DrumPad(int analogPort) {
+    this->analogPort = analogPort;
+  }
+
+  void process() {
+    int currentValue = analogRead(this->analogPort);
+
+    if (volumeEnv.isLocalMaximum(currentValue)) {
+      if (bangEnv.isLocalMaximum(currentValue))
+        outputBang(this->oldValue, this->analogPort);
+      outputVolume(this->oldValue, this->analogPort);
+      this->oldValue = currentValue;
+    }
+  }
+};
+
+DrumPad* drumPads[NUM_PADS];
 
 void setup() {
   // put your setup code here, to run once:
   SLIPSerial.begin(BAUD_RATE);
 
+  for (int i = 0; i < NUM_PADS; i++)
+    drumPads[i] = new DrumPad(i);
+
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  int currentValue = analogRead(A0);
-
-  if (volumeEnv.isLocalMaximum(currentValue)) {
-    if (bangEnv.isLocalMaximum(currentValue)) {
-      outputBang(oldValue);
-    }
-    outputVolume(oldValue);
-    oldValue = currentValue;
-  }
-
+  for (int i = 0; i < NUM_PADS; i++)
+    drumPads[i]->process();
 }
-
-void outputBang(int value) {
-  sendInt(value, "/drum/0/bang");
-}
-
-void outputVolume(int value) {
-  sendInt(value, "/drum/0/volume");
-}
-
-void sendInt(int value, char* address) {
-  if (value < 10)
-    return;
-
-  msg.setAddress(address);
-  msg.add((int32_t)value);
-
-  SLIPSerial.beginPacket();
-  msg.send(SLIPSerial); // send the bytes to the SLIP stream
-  SLIPSerial.endPacket(); // mark the end of the OSC Packet
-  msg.empty(); // free space occupied by message
-}
-
